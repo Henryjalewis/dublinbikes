b4204e7eb97d49773e1b48e2467135ea7f7a5eba
Tom Walsh <tomwalsh96@gmail.com>
2021-02-24 13:46:45 +0000

    Added write_to_file function
    - takes data from API fetch
    - writes to local directory as backup
    - failsafe store of data in case we lose database

--- get_jcdecaux.py
+++ get_jcdecaux.py
@@ -3,10 +3,26 @@ import os
 import requests
 import time
 import datetime
+import os
 
 # Get api params from JSON file
 with open('JCDecaux_key.json') as f:
   JCDecaux_key = json.load(f)
+
+# Function which takes API text data as input and writes to new file in 'data' directory
+def write_to_file(text):
+  # Create 'data' directory if not already exists
+  try:
+    os.stat(os.path.dirname("data"))
+  except:
+    os.mkdir("data")
+  # Write each API call to its own file in 'data' directory
+  with open("data/bikes_{}".format(time.time()).replace(" ", "_"), "w+") as f:
+    f.write(text)
+
+# Function which writes API data to hosted MYSQL database
+def write_to_db(data):
+  pass
 
 def main():
   # Run infinite loop
@@ -21,7 +37,8 @@ def main():
       # Check status code
       if (r.status_code == 200):
         # Handle for success
-        store(json.loads(r.text))
+        write_to_file(r.text)
+        write_to_db(json.loads(r.text))
       elif (r.status_code == 403):
         # Handle for bad parameters error
         print("API parameter error")
@@ -31,8 +48,5 @@ def main():
 
     # Sleep for 5 minutes
     time.sleep(5*60)
-
-def store(data):
-  pass
 
 main()
