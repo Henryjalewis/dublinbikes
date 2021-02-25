ca62841fb6ebdf97f21ac62e0c75c812068deef5
Henryjalewis <57525762+Henryjalewis@users.noreply.github.com>
2021-02-25 21:10:26 +0000

    Delete db.py

--- db.py
+++
@@ -1,6 +0,0 @@
-host = "admin"
-password = "maq57u5s"
-endpoint = "dublinbikes.c4awjd9ragwl.us-east-1.rds.amazonaws.com"
-name = "dublinbikes"
-
-api_key = "6640408cba762550f2ef0404aed7fb84538aeac0"
