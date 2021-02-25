e17bdb27c6ebdc62783a182ca41d0f5fe6e0c1b7
Henry Lewis <henryjalewis@yahoo.co.uk>
2021-02-25 21:06:55 +0000

    flask application

---
+++ db.py
@@ -0,0 +1,6 @@
+host = "admin"
+password = "maq57u5s"
+endpoint = "dublinbikes.c4awjd9ragwl.us-east-1.rds.amazonaws.com"
+name = "dublinbikes"
+
+api_key = "6640408cba762550f2ef0404aed7fb84538aeac0"
