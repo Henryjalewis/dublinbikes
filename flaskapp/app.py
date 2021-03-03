a01b60a4313ff61e310ecc5a2a8b445cb6ca545b
Henry Lewis <henryjalewis@yahoo.co.uk>
2021-03-03 17:16:47 +0000

    updated to show map

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,37 +1,48 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 from jinja2 import Template
 from sqlalchemy import create_engine
-import db
 import pandas as pd
+
+import sys
+
+#sys.path.append('keys.json')
+import json
+
+# Get keys from JSON file
+with open('keys.json') as f:
+    keys = json.load(f)
+
+
 app = Flask(__name__)
 
 @app.route("/")
+# This function sets up the template for the homepage (index)
 def home():
-    return app.send_static_file("index.html")
+    return render_template("index.html")
 
-
+# This function sets up the template for the about page (content and structure to be determined)
 @app.route("/about")
 def about():
     return app.send_static_file("about.html")
 
+# This function sets up the template for the contacts page (content and structure to be determined)
 @app.route("/contact")
 def contact():
     #tpl = Template()
     d = {'name':'Tallguy'}
     return render_template("contact.html", **d)
 
+
+# reads API information for station locations and outputs a dataframe
 @app.route("/stations")
 def stations():
-    engine = create_engine("mysql+mysqlconnector://{host}:{password}@{endpoint}:3306/{db_name}".format(host=db.host,
-                                                                                                        password=db.password,
-                                                                                                        endpoint=db.endpoint,
-                                                                                                        db_name=db.name),
+    engine = create_engine("mysql+mysqlconnector://{host}:{password}@{endpoint}:3306/{db_name}".format(host=keys["db"]["host"],
+                                                                                                        password=keys["db"]["password"],
+                                                                                                        endpoint=keys["db"]["endpoint"],
+                                                                                                        db_name=keys["db"]["name"]),
                                                                                                         echo=True)
     df = pd.read_sql_table("stations", engine)
-    #results = engine.execute("select * from stations")
-    #print([res for res in results])
-    print(df.head())
-    return "nothing"
+    return df.to_json(orient='records')
 
 if __name__ == "__main__":
     app.run(debug=True)
