497d562569143d62340792769192fd2b9d8250fe
Danning Zhan <danning.zhan@hotmail.com>
2021-03-23 09:13:38 +0000

    made a dropdown list, no css

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from jinja2 import Template
 from sqlalchemy import create_engine
 import pandas as pd
@@ -40,6 +40,35 @@ def stations():
                                                                                                         echo=True)
     df = pd.read_sql_table("stations", engine)
     return df.to_json(orient='records')
+
+
+@app.route("/details/<name>")
+def details(name):
+    engine = create_engine("mysql+mysqlconnector://{host}:{password}@{endpoint}:3306/{db_name}".format(host=keys["db"]["host"],
+                                                                                                        password=keys["db"]["password"],
+                                                                                                        endpoint=keys["db"]["endpoint"],
+                                                                                                        db_name=keys["db"]["name"]),
+                                                                                                        echo=True)
+
+    # get the name form javascript
+    #names = request.args.values("station")
+
+    # use the name in a query
+    print(name)
+    #print(request.values.get.keys())
+    query = f"""    
+    select available_bike_stands, available_bikes, max(last_update) from available
+    join stations on available.number = stations.number
+    where stations.name = '{name}'"""
+    print(query)
+    # use the engine connection to query
+    with engine.connect() as con:
+        AV = pd.read_sql_query(query, con)
+
+    print(AV)
+    return AV.to_json(orient='records')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
